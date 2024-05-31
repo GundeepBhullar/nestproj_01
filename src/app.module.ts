909bc5efcb8477c5c70cc8048393369697d869fa
Gundeep bhullar <gundeepbhullar96@gmail.com>
2024-05-31 14:03:28 +0530

    first nest js code

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+//import { AuthModule } from './auth/auth.module';
+import { User } from './user/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot ({
+      type: `postgres`,
+      host: `localhost`,
+      port: 5432,
+      password: `post@123`,
+      username: `postgres`,
+      entities: [User],
+      database: `datanew`,
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+    // AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
